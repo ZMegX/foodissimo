1458d8496c3146d09b4d900c46a3859e3a18051b
ZMegX <picardomegan@gmail.com>
2025-05-24 01:59:43 +0100

    posts mod added

--- board/__init__.py
+++ board/__init__.py
@@ -1,9 +1,12 @@
+# https://realpython.com/videos/scalable-flask-web-app-overview/
 from flask import Flask
-from board import pages
+
+from board import pages, posts
 
 def create_app():
     app = Flask(__name__)
 
     app.register_blueprint(pages.bp)
+    app.register_blueprint(posts.bp)
     return app
 

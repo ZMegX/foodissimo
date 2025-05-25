75872cba4cca119de786ccac9917e2de5a970a7d
ZMegX <picardomegan@gmail.com>
2025-05-25 21:15:38 +0100

    create class post

--- board/models.py
+++ board/models.py
@@ -3,7 +3,9 @@ from board.database import db
 from datetime import datetime
 
 class Post(db.Model):
+    __tablename__ = "post"
+
     id = db.Column(db.Integer, primary_key=True)
-    author = db.Column(db.String(100), default="Anonymous")
+    author = db.Column(db.String(100), nullable=False)
     message = db.Column(db.Text, nullable=False)
     created = db.Column(db.DateTime, default=datetime.utcnow)

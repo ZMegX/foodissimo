c574166ae59dc1effb57faf21139619ed2f5e52f
ZMegX <picardomegan@gmail.com>
2025-05-25 17:08:10 +0100

    import models.py and created db.session to add and commit post by users to the database / SQLAlchemy

--- board/posts.py
+++ board/posts.py
@@ -5,22 +5,20 @@ from flask import (
     redirect, request, url_for
 )
 
-from board.database import get_db
+from board.database import db
+from board.models import Post
 
 bp = Blueprint("posts", __name__)
 
 @bp.route("/create", methods=("GET", "POST"))
 def create():
     if request.method == "POST":
-        author = request.form["author"] or "Anonymous"
-        message = request.form["message"]
+        author = request.form.get["author"] or "Anonymous"
+        message = request.form.get["message"]
         if message:
-            db = get_db()
-            db.execute(
-                "INSERT INTO post (author, message) VALUES (?, ?)",
-                (author, message),
-            )
-            db.commit()
+            new_post = Post(author=author, message=message)          
+            db.session.add(new_post) #with SQLAlchemy we need to us db.session to add/edit
+            db.session.commit()
             return redirect(url_for("posts.posts"))
         
     return render_template("posts/create.html")
@@ -28,8 +26,5 @@ def create():
 
 @bp.route("/posts")
 def posts():
-    db = get_db()
-    posts = db.execute(
-        "SELECT author, message, created FROM post ORDER BY created DESC"
-    ).fetchall()
+    posts = Post.query.order_by(Post.created.desc().all) # query from the post model created
     return render_template("posts/posts.html", posts=posts)

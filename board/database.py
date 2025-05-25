fc03e0f60c19c2b03176d3f0b6decb9a154b3b1b
ZMegX <picardomegan@gmail.com>
2025-05-25 23:39:22 +0100

    config updated

--- board/database.py
+++ board/database.py
@@ -1,6 +1,18 @@
-from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker, declarative_base
 
-db = SQLAlchemy()
+from .config import Config
 
-def init_app(app):
-    db.init_app(app)
+engine = create_engine(Config.SQLALCHEMY_DATABASE_URI)
+db_session = scoped_session(sessionmaker(autocommit=False,
+                                         autoflush=False,
+                                         bind=engine))
+Base = declarative_base()
+Base.query = db_session.query_property()
+
+def init_db():
+  # import all modules here that might define models so that
+  # they will be registered properly on the metadata.  Otherwise
+  # you will have to import them first before calling init_db()
+  import board.models
+  Base.metadata.create_all(bind=engine)

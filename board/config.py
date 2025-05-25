c66218461a91a21c65475119fb5d25526a35ceb9
ZMegX <picardomegan@gmail.com>
2025-05-25 22:22:39 +0100

    url updated with external URL from render

--- board/config.py
+++ board/config.py
@@ -6,5 +6,5 @@ import os
 SECRET_KEY = os.getenv('SECRET_KEY', '0b953105dd30cf778a5b66c91955fed00a363af52639f1a63f7d25429e8306ee')
 
 # When deploying, set in the environment to the PostgreSQL URL
-FLASK_SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://ucd_foodissimo_user:jWomeOu6qJLajSs27jBrOCUz6h0SRIL7@dpg-d0og09mmcj7s73d586g0-a/ucd_foodissimo')
+FLASK_SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://ucd_foodissimo_user:jWomeOu6qJLajSs27jBrOCUz6h0SRIL7@dpg-d0og09mmcj7s73d586g0-a.frankfurt-postgres.render.com/ucd_foodissimo')
 SQLALCHEMY_TRACK_MODIFICATIONS = False

bdaa7919b2233fb64b230879db513188e6aeec8f
ZMegX <picardomegan@gmail.com>
2025-05-25 17:22:16 +0100

    config edited for SQLAlchemy and secret key added

--- board/config.py
+++ board/config.py
@@ -1,11 +1,10 @@
 import os
 
-DATABASE = os.path.join(os.getcwd(), 'db.sqlite3')
 # For a production app, you should use a secret key set in the environment
 # The recommended way to generate a 64char secret key is to run:
 # python -c 'import secrets; print(secrets.token_hex())'
-SECRET_KEY = os.getenv('SECRET_KEY', 'not-set')
+SECRET_KEY = os.getenv('SECRET_KEY', '0b953105dd30cf778a5b66c91955fed00a363af52639f1a63f7d25429e8306ee')
 
 # When deploying, set in the environment to the PostgreSQL URL
-SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://ucd_foodissimo_user:jWomeOu6qJLajSs27jBrOCUz6h0SRIL7@dpg-d0og09mmcj7s73d586g0-a/ucd_foodissimo')
+FLASK_SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://ucd_foodissimo_user:jWomeOu6qJLajSs27jBrOCUz6h0SRIL7@dpg-d0og09mmcj7s73d586g0-a/ucd_foodissimo')
 SQLALCHEMY_TRACK_MODIFICATIONS = False

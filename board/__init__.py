1ad3f22b245c9c93586707a32f663b40d4b4b059
ZMegX <picardomegan@gmail.com>
2025-05-25 23:38:29 +0100

    config edited

--- board/__init__.py
+++ board/__init__.py
@@ -1,16 +1,17 @@
 # https://realpython.com/videos/scalable-flask-web-app-overview/
 import os
-from dotenv import load_dotenv
 from flask import Flask
+from .config import Config
+from .database import init_db
 
 from board import pages, posts, database
 
-load_dotenv()
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_prefixed_env()
-    database.init_app(app)
+    app.config.from_object(Config)  # Load configuration from config.py and particularly the DBMS URI
+
+    init_db()
 
     app.register_blueprint(pages.bp)
     app.register_blueprint(posts.bp)

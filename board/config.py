8e5bb0841cfa2d5679b44e74d4c4f17d7e6b7f77
ZMegX <picardomegan@gmail.com>
2025-05-26 14:29:19 +0100

    fix bug indentation

--- board/config.py
+++ board/config.py
@@ -4,8 +4,8 @@ class Config:
 # For a production app, you should use a secret key set in the environment
 # The recommended way to generate a 64char secret key is to run:
 # python -c 'import secrets; print(secrets.token_hex())'
-SECRET_KEY = os.getenv('SECRET_KEY', '0b953105dd30cf778a5b66c91955fed00a363af52639f1a63f7d25429e8306ee')
+    SECRET_KEY = os.getenv('SECRET_KEY', 'not-set')
 
 # When deploying, set in the environment to the PostgreSQL URL
-FLASK_SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://ucd_foodissimo_user:jWomeOu6qJLajSs27jBrOCUz6h0SRIL7@dpg-d0og09mmcj7s73d586g0-a.frankfurt-postgres.render.com/ucd_foodissimo')
-SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://ucd_foodissimo_user:jWomeOu6qJLajSs27jBrOCUz6h0SRIL7@dpg-d0og09mmcj7s73d586g0-a.frankfurt-postgres.render.com/ucd_foodissimo')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
